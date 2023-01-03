495895f41da46345ed743e292476807f3dbebf14
Son Nguyen <son.nguyen@nw-its.com>
2023-01-03 14:41:48 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello Earth!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
+// git pull origin [branch]
+// git status
+
+// git add --all .
+// git commit -m "Message"
+// git push origin [branch]
