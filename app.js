a96f9a81cd05c43fa40f77da4f3cf2429a8d19f2
Son Nguyen <son.nguyen@nw-its.com>
2023-01-04 12:42:39 -0500

    Updates to the code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Mars!'));
+app.get('/', (req, res) => res.send('Hello All the beautiful people in the office!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
